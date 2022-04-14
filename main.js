0f4f3a1fc2cfd3f342999e9c45cc0a92eb63ebe5
Code-Space <100551018+ViniciusRodrigues2@users.noreply.github.com>
2022-04-14 00:53:39 -0300

    commit inicial

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
-const label = document.querySelector('.label');
-let importText = document.querySelector('.import-text');
-function labelClick(){
-    label.addEventListener('click',()=>{
-       if(label.click){
-          label.style.color = 'blue';
-        }
-        else if(label == label.click){
-            label.addEventListener('click',()=>{
-                alert('oi')
-            })
-        }
-    })
+let corDeFundo = document.querySelector('.denger');
+let text = document.querySelector('.conteudo-header');
+let logo = document.querySelector('.logo');
+let white = document.getElementById('white');
+function mudarCor(){
+    corDeFundo.addEventListener('click',()=>{
+        document.body.style.backgroundColor = '#1B1F24';
+        document.body.style.color = 'white';
+        logo.style.color = '#A23437'
+    });
 }
-labelClick();
+mudarCor();
+function voltar(){
+    white.addEventListener('click',()=>{
+        let cor = 'black'
+        document.body.style.backgroundColor = 'white';
+        document.body.style.color = cor;
+        logo.style.color = cor
+    });
+}
+voltar();
